fc696be8523acb1b2f7d5406409170d895a8cf22
Marius Vollmer <mvollmer@redhat.com>
2016-03-04 13:01:49 +0200

    test: Adapt storage tests to new slider controls

--- test/verify/storagelib.py
+++ test/verify/storagelib.py
@@ -78,6 +78,10 @@ class StorageCase(MachineCase):
         elif isinstance(val, dict):
             for label in val:
                 self.dialog_select(field, label, val[label])
+        elif isinstance(val, int):
+            # size slider
+            self.browser.set_val(self.dialog_field(field) + " select.size-unit", "1048576")
+            self.browser.set_val(self.dialog_field(field) + " input.size-text", str(val))
         else:
             self.browser.set_val(self.dialog_field(field), val)
 
